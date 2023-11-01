7972f52ba6d9147f2712ea11e518fb0709a99065
Jarek Garcia <prodijee@hotmail.com>
2023-10-31 19:47:59 -0600

    env all setup

--- client/src/env.js
+++ client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'jdcode1auth.us.auth0.com'
+export const clientId = '107NIYLd3nMVCVjwHecvRf8fI26E75MM'
+export const audience = 'https://JDcode1.com'
